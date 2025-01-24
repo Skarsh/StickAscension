4addf6e23b274247ed80493669c300fba00572d6
Thomas Skarshaug <skarshaug.thomas@gmail.com>
2025-01-25 00:48:54 +0100

    Cleanup unnecessary camera stuff

--- scripts/game.gd
+++ scripts/game.gd
@@ -13,37 +13,14 @@ extends Node2D
 
 var battle_music = preload("res://music/BattleMusic1.1Cello.mp3")
 
-const SHAKE_LENGTH = 2.0
-
-var shake_duration: float = 0.0
-var shaking = false
-
 var player: Player
 var enemy: Enemy
-
-#func shake_ui():
-#    ui_parent.position = Vector2(
-#        randf_range(-30.0, 30.0),
-#        randf_range(-30.0, 30.0)
-#    )
 
 func _ready() -> void:
 	player = $CanvasLayer/Player
 	enemy = $CanvasLayer/Enemy
 
-func _process(delta: float) -> void:
-	#if Input.is_action_just_pressed("ui_select") and shake_duration < SHAKE_LENGTH:
-	#    print("herro?")
-	#    shaking = true 
-	
-	#if shaking:
-	#    shake_ui()
-	#    shake_duration += delta
-
-	#if shake_duration >= SHAKE_LENGTH:
-	#    shaking = false
-	#    shake_duration = 0
-	
+func _process(delta: float) -> void:	
 	if Input.is_action_just_pressed("ui_select"):
 		player.take_damage(enemy.attack(player.stats))
 		enemy.take_damage(player.attack(enemy.stats))
